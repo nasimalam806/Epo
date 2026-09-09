2caac241c8f4def1f39d79092d4ba281064a100d
nasimalam806 <43523931+nasimalam806@users.noreply.github.com>
2026-09-09 13:10:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Dummy web server start karna (Background me)
-gunicorn web:app --bind 0.0.0.0:$PORT &
+gunicorn web:app --bind 0.0.0.0:${PORT:-10000} &
 # Apna main bot start karna
 python bot.py
