fcb4ffc1a45aa910e1aa55d8b982e4e8d4958290
nasimalam806 <43523931+nasimalam806@users.noreply.github.com>
2026-09-13 13:19:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -181,7 +181,6 @@ def extract_info_only(url, selected_res):
         'format': f'best[height<={selected_res}]', 
         'quiet': True,
         'noplaylist': True,
-        'nocheckcertificate': True,
         'impersonate': ImpersonateTarget.from_str('chrome'),
         'extractor_args': {'youtube': ['player_client=ios,android']},
         'http_headers': {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
